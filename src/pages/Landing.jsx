856513d7c365f48bf523644aea02fde9b6cd3cde
Patrick Castro <patrickcastro117@yahoo.com>
2023-09-22 17:37:51 -0700

    added splash image

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,7 +7,9 @@ function Landing() {
         <Layout >
             <h1 className='bg-blue-600 text-6xl p-5'>The Landing Page</h1>
             <div>
-                <div>SPLASH IMAGE</div>
+                <div className='w-[100%] h-[50rem] overflow-hidden'>
+                    <img src="../splash.jpg" className='w-full'/>
+                </div>
                 <p>New Food. Familiar Place</p>
                 <p>Cook meals from our recipe boxes in the comfort of your own home</p>
             </div> 
