ca076a08f4e3c622e280ee6fee350d2980a91d8e
Patrick Castro <patrickcastro117@yahoo.com>
2023-09-26 16:17:34 -0700

    add carousel

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,32 @@
 import Layout from "../components/Layout";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 import '../index.css';
+import './LandingCarousel.css';
 
 function Landing() {
+
+    const responsive = {
+        superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+        },
+        desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+        },
+        tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+        },
+        mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+        }
+    }
+
     return (
         <Layout >
             {/* <h1 className='bg-blue-600 text-6xl p-5'>The Landing Page</h1> */}
@@ -12,6 +36,15 @@ function Landing() {
                 </div>
                 <p>New Food. Familiar Place</p>
                 <p>Cook meals from our recipe boxes in the comfort of your own home</p>
+                <div>
+                    <Carousel responsive={responsive}>
+                        <div>Item 1</div>
+                        <div>Item 2</div>
+                        <div>Item 3</div>
+                        <div>Item 4</div>
+                        <div>Item 5</div>
+                    </Carousel>
+                </div>
             </div> 
         </Layout>
     )
